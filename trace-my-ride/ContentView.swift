b67006a6bc40baa4e8f95435e24fa054be2d36be
emilio <emiliobarr006@gmail.com>
2021-09-14 00:41:31 -0700

    Initial Commit

---
+++ trace-my-ride/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trace-my-ride
+//
+//  Created by Emilio Barreiro on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trace-my-ride/trace_my_rideApp.swift
@@ -0,0 +1,17 @@
+//
+//  trace_my_rideApp.swift
+//  trace-my-ride
+//
+//  Created by Emilio Barreiro on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct trace_my_rideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
