650fc471f7df7e51bdd500e746ff872c95c65b60
emilio <emiliobarr006@gmail.com>
2021-09-15 01:33:26 -0700

    add register calls+ refactor login/register view

--- trace-my-ride/ContentView.swift
+++ trace-my-ride/ContentView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var email = ""
-    @State var password = ""
     @State var isAuthenticated: Bool = false
-    
-    @State var showSignUp: Bool = false
+
+    var body: some View {
+        NavigationView {
+            LoginView(isAuthenticated: self.$isAuthenticated)
+                .navigationTitle("Sign In")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct LoginView: View {
+    @State var username: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var trackingId: String = ""
+    @State var showRegisterView: Bool = false
+    @State var submitError: Bool = false
+
+    @Binding var isAuthenticated: Bool
     
     func login() {
-        
         let defaults = UserDefaults.standard
-        
         WebService().login(email: email, password: password) { result in
             switch result {
             case .success(let loginResponse):
@@ -36,62 +54,124 @@ struct ContentView: View {
             }
         }
     }
+    
+    func register() {
+        let defaults = UserDefaults.standard
+        WebService().register(username: username, email: email, password: password, trackingId: trackingId) { result in
+            switch result {
+            case .success(let registerResponse):
+                guard let decodedResponse = try? JSONDecoder().decode(RegisterResponse.self, from: registerResponse) else { return }
 
-    var body: some View {
-        NavigationView {
-            VStack {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 150, height: 150)
-                
-                VStack {
-                    TextField("Email Address", text: $email)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(4)
-                        .autocapitalization(.none)
-                        
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(4)
-                    
-                    Button(action: {
-                        login()
-                    }, label: {
-                        Text("Sign In")
-                            .foregroundColor(Color.white)
-                            .frame(width: 200, height: 50)
-                            .background(Color.blue)
-                            .cornerRadius(4)
-                    }).padding()
-                    
-                    HStack {
-                        Text("Don't have an account?")
-                            .foregroundColor(.black)
-                        Button(action: {
-                            self.showSignUp = true
-                        }, label: {
-                            Text("Sign Up")
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                        })
-                    }
+                defaults.setValue(decodedResponse.username, forKey: "username")
+                defaults.setValue(decodedResponse.email, forKey: "email")
+                defaults.setValue(decodedResponse.password, forKey: "password")
+                defaults.setValue(decodedResponse.trackingId, forKey: "trackingId")
+                defaults.setValue(decodedResponse.token, forKey: "token")
+                DispatchQueue.main.async {
+                    self.isAuthenticated = true
                 }
-                .padding()
-                
-                Spacer()
+            case .failure(let error):
+                print(error.localizedDescription)
             }
-            .navigationTitle("Sign In")
-            .sheet(isPresented: $showSignUp) {
-                SignUpView(isPresented: $showSignUp)
-            }
+//            print(defaults.dictionaryRepresentation())
         }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        VStack {
+            VStack {
+                TextField("Email Address", text: $email)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(4)
+                    .autocapitalization(.none)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(4)
+                
+                Button(action: {
+                    login()
+                }, label: {
+                    Text("Sign In")
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(4)
+                }).padding()
+                
+                HStack {
+                    Text("Don't have an account?")
+                        .foregroundColor(.black)
+                    Button(action: {
+                        self.submitError = false
+                        self.showRegisterView.toggle()
+                    }, label: {
+                        Text("Sign Up")
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    })
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .sheet(isPresented: self.$showRegisterView) {
+            NavigationView {
+                VStack {
+                    VStack {
+                        TextField("Username", text: $username)
+                            .autocapitalization(.none)
+                            .padding()
+                            .background(Color(.secondarySystemBackground))
+                            .cornerRadius(4)
+
+                        TextField("Email Address", text: $email)
+                            .autocapitalization(.none)
+                            .padding()
+                            .background(Color(.secondarySystemBackground))
+                            .cornerRadius(4)
+                            
+                        SecureField("Password", text: $password)
+                            .autocapitalization(.none)
+                            .padding()
+                            .background(Color(.secondarySystemBackground))
+                            .cornerRadius(4)
+                        
+                        TextField("Tracking ID", text: $trackingId)
+                            .autocapitalization(.none)
+                            .padding()
+                            .background(Color(.secondarySystemBackground))
+                            .cornerRadius(4)
+                        
+                        Button(action: {
+                            print("checking")
+                            if username == "" || email == "" || password == "" || trackingId == "" {
+                                submitError = true
+                            } else {
+                                submitError = false
+                                self.showRegisterView = false
+                            }
+                        }, label: {
+                            Text("Submit")
+                                .foregroundColor(Color.white)
+                                .frame(width: 200, height: 50)
+                                .background(Color.blue)
+                                .cornerRadius(4)
+                        })
+                        
+                        if submitError {
+                            Text("All fields must be filled out.")
+                                .foregroundColor(.red)
+                                .padding(.top, 5)
+                        }
+                    }
+                    .padding()
+                    
+                    Spacer()
+                }.navigationTitle("Sign Up")
+        }
+        }
     }
 }
--- trace-my-ride/WebService.swift
+++ trace-my-ride/WebService.swift
@@ -17,19 +17,35 @@ struct LoginRequestBody: Codable {
     let password: String
 }
 
-struct LoginResponse: Codable {
-    let _id: String?
-    let username: String?
-    let email: String?
-    let password: String?
-    let trackingId: String?
-    let token: String?
+struct RegisterRequestBody: Codable {
+    let username: String
+    let email: String
+    let password: String
+    let trackingId: String
 }
 
+struct LoginResponse: Codable {
+    public let _id: String?
+    public let __v: Int?
+    public let username: String?
+    public let email: String?
+    public let password: String?
+    public let trackingId: String?
+    public let token: String?
+}
+
+struct RegisterResponse: Codable {
+    public let _id: String?
+    public let __v: Int?
+    public let username: String?
+    public let email: String?
+    public let password: String?
+    public let trackingId: String?
+    public let token: String?
+}
+ 
+
 class WebService: ObservableObject {
-    
-//    func login(email: String, password: String, completion: @escaping (Result<String, AuthenticationError>) -> Void) {
-//    func login(email: String, password: String, completion: @escaping (Result<LoginResponse, AuthenticationError>) -> Void) {
     func login(email: String, password: String, completion: @escaping (Result<Data, AuthenticationError>) -> Void) {
         guard let url = URL(string: "http://192.168.254.68:3000/login") else {
             completion(.failure(.custom(errorMessage: "URL is not correct")))
@@ -48,20 +64,45 @@ class WebService: ObservableObject {
                 completion(.failure(.custom(errorMessage: "no data")))
                 return
             }
+            print("[LOGIN]: data received")
             
             guard let loginResponse = try? JSONDecoder().decode(LoginResponse.self, from: data) else {
                 completion(.failure(.invalidCredentials))
                 return
             }
-            
-//            guard let token = loginResponse.token else {
-//                completion(.failure(.invalidCredentials))
-//                return
-//            }
-            
-//            completion(.success(token))
-            print("here: " + loginResponse.password!)
+            print("[LOGIN]: data decoded")
+            print("[LOGIN] snippet: " + loginResponse.username!)
             completion(.success(data))
         }.resume()
+    }
+    
+    func register(username: String, email: String, password: String, trackingId: String, completion: @escaping (Result<Data, AuthenticationError>) -> Void) {
+        guard let url = URL(string: "http://192.168.254.68:3000/login") else {
+            completion(.failure(.custom(errorMessage: "URL is not correct")))
+            return
+        }
+        
+        let body = RegisterRequestBody(username: username, email: email, password: password, trackingId: trackingId)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data, error == nil else {
+                completion(.failure(.custom(errorMessage: "no data")))
+                return
+            }
+            print("[REGISTER]: data received")
+            
+            guard let registerResponse = try? JSONDecoder().decode(RegisterResponse.self, from: data) else {
+                completion(.failure(.invalidCredentials))
+                return
+            }
+            print("[REGISTER]: data decoded")
+            print("[REGISTER] snippet: " + registerResponse.username!)
+        }
+        
     }
 }
